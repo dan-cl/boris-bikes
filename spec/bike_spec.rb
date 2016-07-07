57e94c17cbcfe6511b5b3cdf4d33cd81f00ee6af
Jack Oddy <jackoddy91@gmail.com>
2016-07-07 13:47:23 +0100

    adds broken method and ivar to bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,25 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :broken }
+
+  describe '#working?' do
+    let(:bike) {Bike.new}
+    it 'confirms bike is working' do
+      expect(bike.working?).to be true
+    end
+    it 'confirms when bike is broken' do
+      expect((bike.broken).working?).to be false
+    end
+  end
+
+
+  describe '#broken' do
+    let(:bike) {Bike.new}
+    it 'flags bike as broken' do
+      bike.broken
+
+      end
+    end
+
 end
