0afcbafc91e3b373e3b89a724f4c11ad88f7eb33
Jack Oddy <jackoddy91@gmail.com>
2016-07-07 10:59:43 +0100

    16 Removing Magic Numbers

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,27 +1,27 @@
 require_relative 'bike'
 
 class DockingStation
+DEFAULT_CAPACITY = 20
 
-def initialize
-  @bikes = []
-end
+  def initialize
+    @bikes = []
+  end
 
+   def release_bike
+     fail "No bikes available" if @bikes.empty?
+     @bikes.pop
+   end
 
- def release_bike
-   fail "No bikes available" if @bikes.empty?
-   @bikes.pop
- end
+   def dock(bike)
+     fail "Docking station full" if full?
+    @bikes << bike
+   end
 
- def dock(bike)
-   fail "Docking station full" if full?
-  @bikes << bike
- end
+   attr_reader :bikes
 
- attr_reader :bikes
+   private
 
- private
-
- def full?
-   @bikes.length >= 20
- end
+   def full?
+     @bikes.length >= DEFAULT_CAPACITY
+   end
 end
